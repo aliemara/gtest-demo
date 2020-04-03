b7971d9d0970c49be8088eaba19e4b3133fc81ae
Ali Emara <emara@adobe.com>
2020-04-03 11:53:41 -0700

    deposit method & tes

--- src/my_classes.h
+++ src/my_classes.h
@@ -5,5 +5,12 @@ public:
 	BankAccount() : balance(0) {}
 	BankAccount(int balance) : balance(balance) {}
 
+	void deposit(int d) {
+		balance += d;
+	}
+
+	const int& getBalance() { return balance; }
+
+private: 
 	int balance;
 };
--- test/my_tests.cpp
+++ test/my_tests.cpp
@@ -4,11 +4,19 @@
 TEST(bank, defaultCtor)
 {
 	BankAccount b;
-	ASSERT_EQ(b.balance, 0);
+	ASSERT_EQ(b.getBalance(), 0);
 }
 
 TEST(bank, paramCtor)
 {
 	BankAccount b(5);
-	ASSERT_EQ(b.balance, 5);
+	ASSERT_EQ(b.getBalance(), 5);
+}
+
+TEST(bank, deposit)
+{
+	int start = 3;
+	BankAccount b(3);
+	b.deposit(start);
+	ASSERT_EQ(b.getBalance(), start*2);
 }
